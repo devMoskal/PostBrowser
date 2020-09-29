ca64296478d383695a696e71490f0a6fc887616b
Moskal <dev.moskal@gmail.com>
2020-09-29 12:21:22 +0200

    remove redundant date support lib

--- app/src/main/java/com/dev/moskal/postbrowser/app/PostBrowserApplication.kt
+++ app/src/main/java/com/dev/moskal/postbrowser/app/PostBrowserApplication.kt
@@ -2,18 +2,15 @@ package com.dev.moskal.postbrowser.app
 
 import android.app.Application
 import com.dev.moskal.postbrowser.BuildConfig
-import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
 open class PostBrowserApplication : Application() {
 
-
     override fun onCreate() {
         super.onCreate()
         initLogs()
-        AndroidThreeTen.init(this)
     }
 
     private fun initLogs() {
