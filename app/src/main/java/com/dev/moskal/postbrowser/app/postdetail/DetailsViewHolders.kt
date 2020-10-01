bbd64fceee4d2407c713b8adee34e8145691f7aa
Moskal <dev.moskal@gmail.com>
2020-10-01 18:46:12 +0200

    show photos using Glide

--- app/src/main/java/com/dev/moskal/postbrowser/app/postdetail/DetailsViewHolders.kt
+++ app/src/main/java/com/dev/moskal/postbrowser/app/postdetail/DetailsViewHolders.kt
@@ -2,12 +2,17 @@ package com.dev.moskal.postbrowser.app.postdetail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
 import com.dev.moskal.postbrowser.R
 import com.dev.moskal.postbrowser.databinding.AlbumItemBinding
 import com.dev.moskal.postbrowser.databinding.DetailsHeaderItemBinding
 import com.dev.moskal.postbrowser.databinding.PhotoItemBinding
-import timber.log.Timber
 
 internal class DetailsHeaderViewHolder(
     parent: ViewGroup,
@@ -33,6 +38,16 @@ internal class AlbumViewHolder(
     }
 }
 
+internal class LoadingViewHolder(
+    parent: ViewGroup,
+) : RecyclerView.ViewHolder(inflateView(parent)) {
+
+    companion object {
+        private fun inflateView(parent: ViewGroup) =
+            LayoutInflater.from(parent.context).inflate(R.layout.post_loading_item, parent, false)
+    }
+}
+
 internal class PhotoViewHolder(
     parent: ViewGroup,
     private val binding: PhotoItemBinding = PhotoItemBinding.inflate(
@@ -42,17 +57,32 @@ internal class PhotoViewHolder(
 
     fun bind(item: DetailsListItem.PhotoItem) {
         binding.item = item
-        Timber.i("### haha")
     }
-}
-
-
-internal class LoadingViewHolder(
-    parent: ViewGroup,
-) : RecyclerView.ViewHolder(inflateView(parent)) {
 
     companion object {
-        private fun inflateView(parent: ViewGroup) =
-            LayoutInflater.from(parent.context).inflate(R.layout.post_loading_item, parent, false)
+        /**
+         * custom binding of image using [Glide]. It's handy solution to load image asynchronously,
+         * aware of ImageView lifecycle and with out-of-the-box cache
+         */
+        @JvmStatic
+        @BindingAdapter("imageUrl")
+        fun loadImage(view: ImageView, url: String?) {
+            if (url == null) return
+
+            // workaround for header issue while using https://via.placeholder.com
+            val glideUrl = GlideUrl(
+                url,
+                LazyHeaders.Builder()
+                    .addHeader(HEADER_KEY_USER_AGENT, WebSettings.getDefaultUserAgent(view.context))
+                    .build()
+            )
+            Glide.with(view.context)
+                .load(glideUrl)
+                .error(R.drawable.photo_placeholder)
+                .placeholder(R.drawable.loading_placeholder)
+                .into(view)
+        }
+
+        private const val HEADER_KEY_USER_AGENT = "User-Agent"
     }
 }
